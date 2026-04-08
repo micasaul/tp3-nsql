5cd95d94725f3e61fd67ba79b8816afb45b2b3db
Mic <mica020804@gmail.com>
2026-04-08 20:51:00 -0300

    Funciones base de GeoRedis

--- backend/app.py
+++ backend/app.py
@@ -2,4 +2,11 @@ from redis import Redis
 
 r = Redis(host='localhost', port=6379, decode_responses=True)
 
-print(r.ping())
+def agregar_lugar(grupo, nombre, lat, lon):
+    r.geoadd(grupo, (lon, lat, nombre))
+
+def buscar_cercanos(grupo, lat, lon):
+    return r.georadius(grupo, lon, lat, 5, unit="km")
+
+def distancia(grupo, lugar1, lugar2):
+    return r.geodist(grupo, lugar1, lugar2, unit="km")
